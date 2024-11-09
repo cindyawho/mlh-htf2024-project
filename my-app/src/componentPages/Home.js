bb3a8ea7a9089dab04d2a72893bd2fcda0b35c41
Cindy Andrade <cindya3939@gmail.com>
2024-11-09 02:47:05 -0800

    Update home page text and styling to match my intended figma design - see Readme

--- my-app/src/componentPages/Home.js
+++ my-app/src/componentPages/Home.js
@@ -1,12 +1,31 @@
 import '../App.css';
+import "./hero.css"
 import React from "react";
 
 function Home() {
   return (
     <header className="App-header">
-        <h2>Welcome to the</h2>
-        <h1>Educator Lounge</h1>
-        <p>HOME PAGE</p>
+      <div class='title'>
+        <div class='titleText'>
+          <h2>Welcome to the</h2>
+          <h1>Educator Lounge</h1>
+        </div>
+        <img 
+          src={require("../assets/shareIdeas.png")}
+          alt="a community with lightbulbs overhead"
+          width="40vw"
+          height="auto"
+        />
+      </div>
+      <div class="subtitle">
+        <img 
+          src={require("../assets/laptop.png")}
+          alt="an illustration of someone working on their laptop"
+          width="40vw"
+          height="auto"
+        />
+        <h2 class='tagline'>Tools, Ideas, and Community <br/> for Today's Teachers</h2>
+      </div>
     </header>
   );
 }
