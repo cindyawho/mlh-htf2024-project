51e004e7efbd14f7f2acba820493063f65d06df7
Cindy Andrade <cindya3939@gmail.com>
2024-11-09 13:00:24 -0800

    Create blog json file with temporary content, create blog component to map onto site

--- my-app/src/componentPages/Blogs.js
+++ my-app/src/componentPages/Blogs.js
@@ -2,6 +2,7 @@ import '../App.css';
 import './blog.css';
 import React from "react";
 import PageHero from '../components/PageHero'
+import blogList from '../data/blogs.json'
 
 function Blogs() {
   return (
@@ -23,10 +24,33 @@ function Blogs() {
         <div class="read">
           <h2>Read</h2>
           <div class="readGrid">
-            
+            {blogList.map((blog, index) => (
+              <BlogItems
+                title={blog.title} 
+                author={blog.author}
+                blogURL={blog.blogURL} 
+                summary={blog.summary}
+              />
+            ))}
           </div>
         </div>
       </div>
+    </>
+  );
+}
+
+function BlogItems({title, author, blogURL, summary}) {
+  return (
+    <>
+    <div class="blogItem">
+      <a href={blogURL} target='_blank' rel='noopener noreferrer'>
+        <div class="itemTitle">
+          <h3>{title}</h3>
+          <h4>{author}</h4>
+        </div>
+        <p>{summary}</p>
+      </a>      
+    </div>
     </>
   );
 }
