241b599c19e71a4feb3827921e25fa5ab9da754b
Cindy Andrade <cindya3939@gmail.com>
2024-11-09 11:08:26 -0800

    Create a component to map resources onto site

--- my-app/src/componentPages/Resources.js
+++ my-app/src/componentPages/Resources.js
@@ -1,6 +1,7 @@
 import '../App.css';
 import React from "react";
 import PageHero from '../components/PageHero';
+import resourceList from '../data/resources.json'
 
 function Resources() {
   return (
@@ -11,7 +12,25 @@ function Resources() {
         subtitle1={"All You Need to Teach,"}
         subtitle2={"All in One Place"}
       />
+      <div class="resourcesMain">
+        {resourceList.map((resource, index) => (
+          <ResourceItems 
+            key={index} 
+            title={resource.title} 
+            fileURL={resource.fileURL} 
+          />
+        ))}
+    </div>
     </>
+  );
+}
+
+function ResourceItems({title, fileURL, imgURL, price, grades, subjects, type, format, size}) {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>{fileURL}</p>
+    </div>
   );
 }
 
