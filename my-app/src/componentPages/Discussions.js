f01758be8da722ec66b8033b226d6fc7fe53f952
Cindy Andrade <cindya3939@gmail.com>
2025-01-10 09:54:49 -0800

    Replace temporary image with a dynamic discussions board

--- my-app/src/componentPages/Discussions.js
+++ my-app/src/componentPages/Discussions.js
@@ -5,6 +5,7 @@ import React from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel'; // yarn add react-responsive-carousel
 import Button from '../components/Button';
+import discussionList from '../data/discussions.json'
 
 function Discussions() {
   return (
@@ -41,11 +42,50 @@ function Discussions() {
       <Button 
         text="Start a Discussion"
       />
-      <img 
-        src={require("../assets/temporary/DiscussionsOrganization.png")} 
-        alt="static visual of discussions"
-        class="tempDiscussions"
-      />
+
+      <div class="discussionContainer">
+        <div class="filterBar">
+          <p>Sort By: </p>
+          <select name="filter" id="filter">
+            <option value="newest">Newest</option>
+            <option value="oldest">Oldest</option>
+            <option value="popular">Popular</option>
+          </select>
+          <p>Filter By: </p>
+          <select name="subject" id="subject">
+            <option value="math">Subjects</option>
+          </select>
+          <select name="grades" id="grades">
+            <option value="kinder">Grades</option>
+          </select>
+        </div>
+        <hr/>
+        <div class="discussions">
+          {discussionList.map((item, index) => (
+            <DiscussionItems
+              title={item.title} 
+              author={item.poster}
+              date={item.date} 
+              replies={item.replies}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+function DiscussionItems({title, author, date, replies}) {
+  return (
+    <>
+    <div class="discussion">
+      <div class="discussionHeader">
+        <p>Posted by: {author}</p>
+        <p>{date}</p>
+      </div>
+      <h2>{title}</h2>
+      <p class="discussionReplies">Replies: {replies}</p>
+    </div>
     </>
   );
 }
