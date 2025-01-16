a372c544be18978e4b3c2f629a547df85772c1a3
Cindy Andrade <cindya3939@gmail.com>
2025-01-16 14:22:44 -0800

    Integrate googleOAuth to make sign in possible and style new sign in page

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId={clientId}>
+      <React.StrictMode>
+          <App />
+      </React.StrictMode>
+  </GoogleOAuthProvider>
 );
